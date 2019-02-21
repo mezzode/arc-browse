54960d619f16231ae88be04b9b9afff2fc005c3b
Sean <toranos@gmail.com>
2019-02-21 22:09:14 +1100

    Add tests

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,28 @@
 import { commands, ExtensionContext, window, workspace } from 'vscode';
 
+export const getPath = (useRelative: boolean): string => {
+  const editor = window.activeTextEditor;
+  if (editor === undefined) {
+    const message = 'No document open.';
+    window.showErrorMessage(message);
+    throw Error(message);
+  }
+
+  const uri = editor.document.uri;
+  if (uri.scheme !== 'file') {
+    const message = 'Open document is not a file.';
+    window.showErrorMessage(message);
+    throw Error(message);
+  }
+
+  return useRelative ? workspace.asRelativePath(uri) : uri.fsPath;
+};
+
 export const activate = (context: ExtensionContext) => {
   const disposable = commands.registerCommand('extension.arc-browse', () => {
-    const editor = window.activeTextEditor;
-    if (editor === undefined) {
-      window.showErrorMessage('No document open.');
-
-      return;
-    }
-
-    const uri = editor.document.uri;
-    if (uri.scheme !== 'file') {
-      window.showErrorMessage('Open document is not a file.');
-
-      return;
-    }
-
     const config = workspace.getConfiguration('arc-browse');
     const useRelative = config.get<boolean>('relative', true);
-    const path = useRelative ? workspace.asRelativePath(uri) : uri.fsPath;
+    const path = getPath(useRelative);
 
     const term = window.createTerminal('arc browse');
     term.sendText(`arc browse ${path}`);
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,21 +1,28 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
+// tslint:disable: await-promise
 import * as assert from 'assert';
+import * as path from 'path';
+import { Uri, window, workspace } from 'vscode';
+import { getPath } from '../extension';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
-
-// Defines a Mocha test suite to group tests of similar kind together
 suite('Extension Tests', () => {
-  // Defines a Mocha unit test
-  test('Something 1', () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  test('Relative path works', async () => {
+    const testPath = Uri.file(path.join(__dirname, '../../src/test/index.ts'));
+    const document = await workspace.openTextDocument(testPath);
+    await window.showTextDocument(document);
+
+    const useRelative = true;
+    assert.strictEqual(
+      getPath(useRelative),
+      workspace.asRelativePath(testPath),
+    );
+  });
+
+  test('Absolute path works', async () => {
+    const testPath = Uri.file(path.join(__dirname, '../../src/test/index.ts'));
+    const document = await workspace.openTextDocument(testPath);
+    await window.showTextDocument(document);
+
+    const useRelative = false;
+    assert.strictEqual(getPath(useRelative), testPath.fsPath);
   });
 });
