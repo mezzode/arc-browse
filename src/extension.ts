b42d3bcd4c0bd8f33cadb2c69bc9bc44247bbe9d
Sean <toranos@gmail.com>
2019-02-21 20:46:57 +1100

    Configure linter and fix lint issues

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,29 @@
-import { commands, window, workspace, ExtensionContext } from 'vscode';
+import { commands, ExtensionContext, window, workspace } from 'vscode';
 
-export function activate(context: ExtensionContext) {
-    let disposable = commands.registerCommand('extension.arc-browse', () => {
-        const editor = window.activeTextEditor;
-        if (!editor) {
-            window.showErrorMessage('No document open.');
-            return;
-        }
-        
-        const uri = editor.document.uri;
-        if (uri.scheme !== 'file') {
-            window.showErrorMessage('Open document is not a file.');
-            return;
-        }
-        
-        const config = workspace.getConfiguration('arc-browse');
-        const useRelative = config.get<boolean>('relative', true);
-        const path = useRelative ? workspace.asRelativePath(uri) : uri.fsPath;
-        
-        const term = window.createTerminal('arc browse');
-        term.sendText(`arc browse ${path}`);
-        term.show();
-    });
-    
-    context.subscriptions.push(disposable);
-}
+export const activate = (context: ExtensionContext) => {
+  const disposable = commands.registerCommand('extension.arc-browse', () => {
+    const editor = window.activeTextEditor;
+    if (editor === undefined) {
+      window.showErrorMessage('No document open.');
 
-export function deactivate() {}
+      return;
+    }
+
+    const uri = editor.document.uri;
+    if (uri.scheme !== 'file') {
+      window.showErrorMessage('Open document is not a file.');
+
+      return;
+    }
+
+    const config = workspace.getConfiguration('arc-browse');
+    const useRelative = config.get<boolean>('relative', true);
+    const path = useRelative ? workspace.asRelativePath(uri) : uri.fsPath;
+
+    const term = window.createTerminal('arc browse');
+    term.sendText(`arc browse ${path}`);
+    term.show();
+  });
+
+  context.subscriptions.push(disposable);
+};
